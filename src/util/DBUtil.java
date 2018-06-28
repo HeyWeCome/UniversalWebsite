ef2d53862378796405dca7d342086f44936d7d67
heywecome <heywecome@163.com>
2018-06-28 09:25:03 +0800

    连接数据库配置修改

--- src/util/DBUtil.java
+++ src/util/DBUtil.java
@@ -9,56 +9,28 @@ import java.sql.Statement;
 
 public class DBUtil {
 
+	// 连接数据库
 	public static Connection getConnection() throws Exception{
 		String driverName = "com.mysql.jdbc.Driver";
-		//数据库URL
-		String url = "jdbc:mysql://localhost:3306/questionnaire?useUnicode=true&characterEncoding=UTF-8";//使数据库存入中文时不乱码
+		String url = "jdbc:mysql://localhost:3306/questionnaire";
+		
+		// 换成你们各自对应的账号密码
 		String userName = "root";
-		String userPWD = "";
-		
-		Class.forName(driverName);//启动
-		
-		Connection connection = DriverManager.getConnection(url,userName,userPWD);//获得连接
-		
-		return connection;
+		String userPWD = "123456";
+
+		Class.forName(driverName);
+
+		Connection conn = DriverManager.getConnection(url,userName,userPWD);
+
+		return conn;
 	}
-	
+
 	public static boolean execute(String sql) throws Exception{
 		Connection connection = getConnection();
-		
-		Statement  statement =  connection.createStatement();
-		
+
+		Statement statement = connection.createStatement();
+
 		return statement.execute(sql);
-	}
-	
-	public static ResultSet list(String sql) throws Exception{
-		Connection connection = getConnection();
-		ResultSet result = null;	
-		
-		/*try {
-			Statement statement = connection.createStatement();
-			statement.executeQuery(sql);
-		}
-		catch (SQLException e) {
-			e.printStackTrace();
-		}*/
-		
-		try {
-			PreparedStatement preparedStatement =  connection.prepareStatement(sql);
-			result = preparedStatement.executeQuery();//得到数据库中所有的数据
-		}
-		catch (SQLException e) {
-			e.printStackTrace();
-			
-		}
-		/*finally{
-			try {
-					connection.close();
-	             } catch (SQLException e) {
-	                // TODO Auto-generated catch block
-	                e.printStackTrace();
-	            }
-		}*/
-		return result;
+
 	}
 }
