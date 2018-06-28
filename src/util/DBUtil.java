e899f7043e5502a8a5f7fe05847b3410f0d967b5
lmy <1048542030@qq.com>
2018-06-28 14:10:27 +0800

    配置文件

--- src/util/DBUtil.java
+++ src/util/DBUtil.java
@@ -12,11 +12,11 @@ public class DBUtil {
 	// 连接数据库
 	public static Connection getConnection() throws Exception{
 		String driverName = "com.mysql.jdbc.Driver";
-		String url = "jdbc:mysql://localhost:3306/questionnaire";
+		String url = "jdbc:mysql://localhost:3306/universalwebsite?useUnicode=true&characterEncoding=UTF-8";
 		
 		// 换成你们各自对应的账号密码
 		String userName = "root";
-		String userPWD = "123456";
+		String userPWD = "";
 
 		Class.forName(driverName);
 
@@ -32,5 +32,21 @@ public class DBUtil {
 
 		return statement.execute(sql);
 
+	}
+	
+	public static ResultSet returnList(String sql) throws Exception{
+		Connection connection = getConnection();
+		ResultSet result = null;	
+		
+		try {
+			PreparedStatement preparedStatement =  connection.prepareStatement(sql);
+			result = preparedStatement.executeQuery();//得到数据库中所有的数据
+		}
+		catch (SQLException e) {
+			e.printStackTrace();
+			
+		}
+		
+		return result;
 	}
 }
