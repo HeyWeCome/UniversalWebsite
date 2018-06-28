5b3fd2fe39eea7a87f7afe8c35d67b3151d42e5b
heywecome <heywecome@163.com>
2018-06-28 11:06:06 +0800

    模块管理service层定义

---
+++ src/service/IModuleManage.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package service;
+
+/** 
+ * @ClassName:     IModuleManage.java 
+ * @Description:   模块管理的接口
+ * @author         Vico.Ho 
+ * @version        V1.0   
+ * @Date           2018年6月28日 上午11:01:56  
+ */
+public interface IModuleManage {
+
+}
