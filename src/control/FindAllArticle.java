3089cffff1e0a6a5286a368725d2882854c582f0
heywecome <heywecome@163.com>
2018-06-28 15:36:38 +0800

    实体类内容修改

---
+++ src/control/FindAllArticle.java
@@ -0,0 +1,35 @@
+/**
+ * 
+ */
+package control;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/** 
+ * @ClassName:     FindAllArticle.java 
+ * @Description:   TODO(用一句话描述该文件做什么)  
+ * @author         Vico.Ho 
+ * @version        V1.0   
+ * @Date           2018年6月28日 下午3:32:06  
+ */
+public class FindAllArticle extends HttpServlet {
+
+
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+
+	}
+
+
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+
+	}
+
+}
--- src/entity/SonColumns.java
+++ src/entity/SonColumns.java
@@ -1,21 +1,25 @@
+/**
+ * 
+ */
 package entity;
 
-/**
- * @ClassName:     Column.java 
- * @Description:   栏目表
+/** 
+ * @ClassName:     SonColumns.java 
+ * @Description:   子栏目类 
  * @author         Vico.Ho 
  * @version        V1.0   
- * @Date           2018年6月27日 下午9:46:38
+ * @Date           2018年6月28日 下午3:34:07  
  */
-public class Columns {
+public class SonColumns {
 	private Integer id;
 	private String name;
 	private String level;
+	private Integer parentID;
 	
 	/**
 	 * 默认构造方法
 	 */
-	public Columns() {
+	public SonColumns() {
 		super();
 	}
 
@@ -26,11 +30,12 @@ public class Columns {
 	 * @param level
 	 * @param parent
 	 */
-	public Columns(Integer id, String name, String level) {
+	public SonColumns(Integer id, String name, String level, Integer parentID) {
 		super();
 		this.id = id;
 		this.name = name;
 		this.level = level;
+		this.parentID = parentID;
 	}
 
 	public Integer getId() {
@@ -55,6 +60,14 @@ public class Columns {
 
 	public void setLevel(String level) {
 		this.level = level;
+	}
+
+	public Integer getParentID() {
+		return parentID;
+	}
+
+	public void setParentID(Integer parentID) {
+		this.parentID = parentID;
 	}
 
 	
