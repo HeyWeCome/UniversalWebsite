9dd47171b67d9727f71a4ea309a1714ee8a0bf7b
heywecome <heywecome@163.com>
2018-06-29 10:47:20 +0800

    文件上传测试案例实现

--- src/control/FindAllColumns.java
+++ src/control/FindAllColumns.java
@@ -4,7 +4,6 @@
 package control;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -14,7 +13,6 @@ import javax.servlet.http.HttpServletResponse;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONArray;
 
-import service.article.ArticleManage;
 import service.columns.ColumnsManage;
 
 /** 
